2f0bef0b5e7febbccc5b4681df4ed824f68577c2
Akshat Dubey <akshatdubey0808@gmail.com>
2024-03-26 17:22:58 +0530

    Removed passport dependencies

--- Backend/Middleware/checkIfAuthenticated.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-export async function checkIfAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect(process.env.CLIENT_URL);
-}
--- Backend/index.js
+++ Backend/index.js
@@ -3,15 +3,12 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const express = require("express");
 const cors = require("cors");
-const session = require("express-session");
-const passport = require("passport");
 const DbConnect = require("./Config/database");
-const googleAuth = require("./Passport/GoogleAuth");
 const apiv1Router = require("./Routes/IndexRouter");
 
 const app = express();
 
-app.enable('trust proxy')
+app.enable("trust proxy");
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
@@ -28,20 +25,4 @@ app.get("/", (req, res) => {
 
 DbConnect();
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    cookie: {
-      secure: true,
-      httpOnly: false,
-      sameSite: "none",
-      maxAge: 15 * 24 * 60 * 60 * 1000,
-    },
-    saveUninitialized: true,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
 app.use("/api/v1", apiv1Router);
