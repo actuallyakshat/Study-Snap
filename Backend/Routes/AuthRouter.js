0ca8813aa0986be02fd01718e21b5479f5f7d975
Akshat Dubey <akshatdubey0808@gmail.com>
2024-03-26 17:26:23 +0530

    Removed passport auth routes

--- Backend/Routes/AuthRouter.js
+++ Backend/Routes/AuthRouter.js
@@ -1,38 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
 const { getDetails, deleteAccount } = require("../Controllers/AuthController");
 require("dotenv").config();
-
-router.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-router.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: `${process.env.CLIENT_URL}/`,
-  }),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.redirect(`${process.env.CLIENT_URL}/dashboard`);
-  }
-);
-
-router.get("/check", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.send({ user: req.user });
-  } else {
-    res.send({ user: null });
-  }
-});
-
-router.post("/logout", (req, res, next) => {
-  req.session.destroy((err) => {
-    res.json({ message: "Logged out!" });
-  });
-});
 
 router.post("/getDetails", getDetails);
 router.delete("/delete-account", deleteAccount);
