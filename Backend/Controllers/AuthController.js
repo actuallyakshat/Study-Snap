95fae8f3284c2369c1de1e69ea657ee8c3f5bb0b
Akshat Dubey <akshatdubey0808@gmail.com>
2024-04-10 00:01:14 +0530

    Fixed: Streak logic. Added: Pari's testimonial

--- Backend/Controllers/AuthController.js
+++ Backend/Controllers/AuthController.js
@@ -33,6 +33,33 @@ const getDetails = async (req, res) => {
     }
     const productivityData = await ProductivityData.find({ email });
 
+    const currentDate = new Date();
+    const todayDateString = `${("0" + currentDate.getDate()).slice(-2)}/${(
+      "0" +
+      (currentDate.getMonth() + 1)
+    ).slice(-2)}/${currentDate.getFullYear()}`;
+    const previousDate = new Date();
+    previousDate.setDate(previousDate.getDate() - 1); // Get the previous date
+
+    const previousDay = ("0" + previousDate.getDate()).slice(-2); // Add leading zero if needed
+    const previousMonth = ("0" + (previousDate.getMonth() + 1)).slice(-2); // Add leading zero if needed
+    const previousYear = previousDate.getFullYear();
+
+    const previousDateString = `${previousDay}/${previousMonth}/${previousYear}`;
+    const previousProductivityData = productivityData.find(
+      (entry) => entry.date === previousDateString
+    );
+    const todaysProductivityData = productivityData.find(
+      (entry) => entry.date === todayDateString
+    );
+
+    if (!previousProductivityData) {
+      if (!todaysProductivityData) {
+        user.streak = 0;
+        await user.save();
+      }
+    }
+
     const startOfWeek = new Date();
     startOfWeek.setHours(0, 0, 0, 0);
     startOfWeek.setDate(
@@ -118,6 +145,149 @@ const getDetails = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+// const getDetails = async (req, res) => {
+//   try {
+//     const { email, name } = req.body;
+
+//     let user = await User.findOne({ email })
+//       .populate({
+//         path: "todos",
+//         options: { sort: { order: 1 } },
+//       })
+//       .populate({
+//         path: "completedTimers",
+//         options: { sort: { _id: -1 } },
+//       })
+//       .populate({
+//         path: "folders",
+//         populate: { path: "notes" },
+//       });
+
+//     if (!user) {
+//       user = new User({
+//         email,
+//         name,
+//       });
+//       await user.save();
+//     }
+
+//     // Fetch productivity data for the user
+//     const productivityData = await ProductivityData.find({ email });
+
+//     //Streak Logic
+// const currentDate = new Date();
+// const todayDateString = `${("0" + currentDate.getDate()).slice(-2)}/${(
+//   "0" +
+//   (currentDate.getMonth() + 1)
+// ).slice(-2)}/${currentDate.getFullYear()}`;
+// const previousDate = new Date();
+// previousDate.setDate(previousDate.getDate() - 1); // Get the previous date
+
+// const previousDay = ("0" + previousDate.getDate()).slice(-2); // Add leading zero if needed
+// const previousMonth = ("0" + (previousDate.getMonth() + 1)).slice(-2); // Add leading zero if needed
+// const previousYear = previousDate.getFullYear();
+
+// const previousDateString = `${previousDay}/${previousMonth}/${previousYear}`;
+// const previousProductivityData = productivityData.find(
+//   (entry) => entry.date === previousDateString
+// );
+// const todaysProductivityData = productivityData.find(
+//   (entry) => entry.date === todayDateString
+// );
+
+// if (!previousProductivityData) {
+//   if (!todaysProductivityData) {
+//     user.streak = 0;
+//     await user.save();
+//   }
+// }
+
+//     // Initialize arrays for weekly, monthly, and yearly summaries
+//     const weeklySummary = [
+//       { day: "Monday", hours: 0 },
+//       { day: "Tuesday", hours: 0 },
+//       { day: "Wednesday", hours: 0 },
+//       { day: "Thursday", hours: 0 },
+//       { day: "Friday", hours: 0 },
+//       { day: "Saturday", hours: 0 },
+//       { day: "Sunday", hours: 0 },
+//     ];
+
+//     // Initialize monthly summary as an array for the current month
+//     const today = new Date();
+//     const currentYear = today.getFullYear();
+//     const currentMonth = today.getMonth() + 1; // Month is 0-indexed
+//     const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
+//     const monthlySummary = new Array(daysInMonth).fill(0).map((_, index) => ({
+//       day: index + 1,
+//       hours: 0,
+//     }));
+
+//     // Initialize yearly summary as an array of objects
+//     const yearlySummary = Array.from({ length: 12 }, (_, i) => ({
+//       month: new Date(0, i).toLocaleString("default", { month: "long" }),
+//       hours: 0,
+//     }));
+
+//     // Calculate start and end dates for the current week (Monday to Sunday)
+//     const startOfWeek = new Date(currentDate);
+//     startOfWeek.setDate(startOfWeek.getDate() - (startOfWeek.getDay() - 1)); // Start of current week (Monday)
+//     const endOfWeek = new Date(currentDate);
+//     endOfWeek.setDate(startOfWeek.getDate() + 6); // End of current week (Sunday)
+
+//     // Filter productivity data to include only entries from the current ongoing week
+//     const currentWeekData = productivityData.filter((entry) => {
+//       const entryDate = new Date(entry.date);
+//       return entryDate >= startOfWeek && entryDate <= endOfWeek;
+//     });
+
+//     // Iterate through filtered productivity data to update weekly summary
+//     currentWeekData.forEach((entry) => {
+//       const { date, hoursStudied } = entry;
+//       const entryDate = new Date(date);
+//       let dayOfWeek = entryDate.getDay(); // 0 for Sunday, 1 for Monday, etc.
+//       if (dayOfWeek === 0) dayOfWeek = 7; // Convert Sunday to 7
+//       weeklySummary[dayOfWeek - 1].hours += hoursStudied; // Adjust index to start from 0
+//     });
+
+//     // Iterate through productivity data to calculate summaries
+//     productivityData.forEach((entry) => {
+//       const { date, hoursStudied } = entry;
+//       const [day, month, year] = date.split("/"); // Split the date string
+
+//       // Update monthly summary if the entry belongs to the current month
+//       if (parseInt(month) === currentMonth && parseInt(year) === currentYear) {
+//         const dayOfMonth = parseInt(day); // Get the day of the month
+//         monthlySummary[dayOfMonth - 1].hours += hoursStudied;
+//       }
+
+//       // Update yearly summary
+//       const monthName = new Date(`${year}-${month}-${day}`).toLocaleString(
+//         "default",
+//         { month: "long" }
+//       );
+//       const monthIndex = yearlySummary.findIndex(
+//         (month) => month.month === monthName
+//       );
+//       yearlySummary[monthIndex].hours += hoursStudied;
+//     });
+
+//     res.status(200).json({
+//       user: {
+//         ...user.toObject(),
+//         productivityData: {
+//           Weekly: weeklySummary,
+//           Monthly: monthlySummary,
+//           Yearly: yearlySummary,
+//         },
+//       },
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// };
 
 const deleteAccount = async (req, res) => {
   try {
--- Frontend/src/Components/Home/TestimonialData.js
+++ Frontend/src/Components/Home/TestimonialData.js
@@ -1,21 +1,21 @@
 const data = [
   {
     name: "Nehal Aggarwal",
-    course: "BCOM ECO",
+    course: "B.COM ECO",
     review:
       "I've been using StudySnap for a while now, and it's completely changed my study routine. I can simply manage my study hours and goals using its user-friendly design, which includes weekly, monthly, and annual study graphs. But what actually distinguishes StudySnap is its all-encompassing capabilities. From to-do lists to built-in notes and Pomodoro trackers, I have everything I need right here. It has helped me study more efficiently and effectively. Highly recommended.",
     image: "/testimonialImages/nehal.png",
   },
   {
     name: "Dhruv Kaushik",
-    course: "BTECH CSE",
+    course: "B.TECH CSE",
     review:
       "Study snap is a very productive platform and unique of its kind. It gives you all the tools to be more productive and take charge of your time. Very thoughtfully made by Akshat.",
     image: "/testimonialImages/dhruv.jpg",
   },
   {
     name: "Om Chauhan",
-    course: "BTECH CSE",
+    course: "B.TECH CSE",
     review:
       "This productivity tracker has been a game-changer for me. With features like the pomodoro timer, note-taking, and insightful visualizations, I've been able to track my study sessions effectively. The intuitive interface and seamless user experience make it a pleasure to use daily. I'm truly impressed by the impact it's had on my productivity and would highly recommend it to anyone looking to maximize their study time.",
     image: "/testimonialImages/om.jpg",
@@ -26,6 +26,13 @@ const data = [
     review:
       "Study Snap is a great website for keeping a track of your day to day progress, it is a one stop destination for people who want to be at their best versions everyday. It is so minimal and user-friendly that any user can easily keep a track of their activities. My personal favourite is the productivity tracker which keeps me motivated over time, along with all the flawless tools and seemless editing done!",
     image: "/testimonialImages/sneha.jpg",
+  },
+  {
+    name: "Pari Singh Chauhan",
+    course: "B.TECH CSE",
+    review:
+      "StudySnap has truly transformed my daily progress tracking. Its intuitive interface and simplicity streamline my productivity journey effortlessly. I particularly appreciate its concise layout, ensuring I stay focused on my goals. However, what truly distinguishes it is the Productivity Tracker feature, which not only motivates me but also offers a visual representation of my advancement. For anyone seeking a no-fuss productivity companion, StudySnap is a must-try",
+    image: "/testimonialImages/pari.jpg",
   },
 ];
 
