4a0640cdd005008b3179d471a9b728ed44ce9f90
Akshat Dubey <akshatdubey0808@gmail.com>
2024-04-19 17:36:16 +0530

    Fix: Disabled zen mode on initial load

--- Frontend/src/Components/Whiteboard/WhiteboardLayout.jsx
+++ Frontend/src/Components/Whiteboard/WhiteboardLayout.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
 import _ from "lodash";
 
@@ -10,25 +10,44 @@ export const WhiteboardLayout = () => {
   // Function to handle the change event with debouncing
   const handleChangeDebounced = _.debounce(
     (excalidrawElements, appState, files) => {
+      // Update the elements state with the new excalidrawElements
       setElements(excalidrawElements);
+
+      // Store the elements array in local storage
       localStorage.setItem(
         "excalidrawElements",
         JSON.stringify(excalidrawElements)
       );
+
+      // Store the files array in local storage
       localStorage.setItem("excalidrawFiles", JSON.stringify(files));
+
+      // Update the savedFiles state with the new files
       setSavedFiles(files);
     },
-    300
+    2000
   ); // Debounce for 2 seconds (2000 milliseconds)
+
+  useEffect(() => {
+    console.log(JSON.parse(localStorage.getItem("excalidrawElements")) || []);
+    console.log(JSON.parse(localStorage.getItem("excalidrawFiles")) || []);
+  }, []);
 
   return (
     <div className="w-full h-full custom-styles">
+      <style>
+        {`
+          .excalidraw-text {
+            font-family: 'Arial', sans-serif; /* Change the font family here */
+          }
+        `}
+      </style>
       <Excalidraw
         onChange={handleChangeDebounced}
         theme="light"
         initialData={{
           appState: {
-            zenModeEnabled: true,
+            zenModeEnabled: false,
             viewBackgroundColor: "#0d1117",
             currentItemStrokeColor: "#fff",
           },
