f4af7b812d7ada6d6e5d89a7277765c61124a5c9
Akshat Dubey <akshatdubey0808@gmail.com>
2024-04-19 17:24:44 +0530

    Feature: The whiteboard saves the content locally

--- Frontend/src/Components/Whiteboard/WhiteboardLayout.jsx
+++ Frontend/src/Components/Whiteboard/WhiteboardLayout.jsx
@@ -1,11 +1,38 @@
+import React, { useEffect, useState } from "react";
 import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
+
 export const WhiteboardLayout = () => {
+  // Initialize state to hold the excalidrawElements and files
+  const [elements, setElements] = useState([]);
+  const [savedFiles, setSavedFiles] = useState([]);
+
+  // Function to handle the change event
+  const handleChange = (excalidrawElements, appState, files) => {
+    // Update the elements state with the new excalidrawElements
+    setElements(excalidrawElements);
+
+    // Store the elements array in local storage
+    localStorage.setItem(
+      "excalidrawElements",
+      JSON.stringify(excalidrawElements)
+    );
+
+    // Store the files array in local storage
+    localStorage.setItem("excalidrawFiles", JSON.stringify(files));
+
+    // Update the savedFiles state with the new files
+    setSavedFiles(files);
+  };
+
+  useEffect(() => {
+    console.log(JSON.parse(localStorage.getItem("excalidrawElements")) || []);
+    console.log(JSON.parse(localStorage.getItem("excalidrawFiles")) || []);
+  }, []);
+
   return (
     <div className="w-full h-full custom-styles">
       <Excalidraw
-        onChange={() => {
-          console.log("Change");
-        }}
+        onChange={handleChange}
         theme="light"
         initialData={{
           appState: {
@@ -14,10 +41,12 @@ export const WhiteboardLayout = () => {
             currentItemStrokeColor: "#fff",
           },
           scrollToContent: true,
+          elements:
+            JSON.parse(localStorage.getItem("excalidrawElements")) || [],
+          files: JSON.parse(localStorage.getItem("excalidrawFiles")) || [],
         }}
       >
         <MainMenu>
-          <MainMenu.DefaultItems.ClearCanvas />
           <MainMenu.DefaultItems.SaveAsImage />
         </MainMenu>
         <WelcomeScreen>
