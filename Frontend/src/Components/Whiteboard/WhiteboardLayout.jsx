1ad6b8ce6b082bdaf5076564db00f6e90db15669
Akshat Dubey <akshatdubey0808@gmail.com>
2024-04-19 17:30:19 +0530

    Enhancement: Added some slight debouncing on local storage saves

--- Frontend/src/Components/Whiteboard/WhiteboardLayout.jsx
+++ Frontend/src/Components/Whiteboard/WhiteboardLayout.jsx
@@ -1,38 +1,30 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
+import _ from "lodash";
 
 export const WhiteboardLayout = () => {
   // Initialize state to hold the excalidrawElements and files
   const [elements, setElements] = useState([]);
   const [savedFiles, setSavedFiles] = useState([]);
 
-  // Function to handle the change event
-  const handleChange = (excalidrawElements, appState, files) => {
-    // Update the elements state with the new excalidrawElements
-    setElements(excalidrawElements);
-
-    // Store the elements array in local storage
-    localStorage.setItem(
-      "excalidrawElements",
-      JSON.stringify(excalidrawElements)
-    );
-
-    // Store the files array in local storage
-    localStorage.setItem("excalidrawFiles", JSON.stringify(files));
-
-    // Update the savedFiles state with the new files
-    setSavedFiles(files);
-  };
-
-  useEffect(() => {
-    console.log(JSON.parse(localStorage.getItem("excalidrawElements")) || []);
-    console.log(JSON.parse(localStorage.getItem("excalidrawFiles")) || []);
-  }, []);
+  // Function to handle the change event with debouncing
+  const handleChangeDebounced = _.debounce(
+    (excalidrawElements, appState, files) => {
+      setElements(excalidrawElements);
+      localStorage.setItem(
+        "excalidrawElements",
+        JSON.stringify(excalidrawElements)
+      );
+      localStorage.setItem("excalidrawFiles", JSON.stringify(files));
+      setSavedFiles(files);
+    },
+    300
+  ); // Debounce for 2 seconds (2000 milliseconds)
 
   return (
     <div className="w-full h-full custom-styles">
       <Excalidraw
-        onChange={handleChange}
+        onChange={handleChangeDebounced}
         theme="light"
         initialData={{
           appState: {
