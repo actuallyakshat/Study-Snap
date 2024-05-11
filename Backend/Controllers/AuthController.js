ae18051273e5c4a0afa8d9a42d174cd89ad46b45
Akshat Dubey <akshatdubey0808@gmail.com>
2024-05-11 12:43:06 +0530

    Removed: Log messages and pre-refactor code

--- Backend/Controllers/AuthController.js
+++ Backend/Controllers/AuthController.js
@@ -2,152 +2,6 @@ const User = require("../Models/User");
 const ProductivityData = require("../Models/ProductivityData");
 const Note = require("../Models/Note");
 
-//before refactoring
-// const getDetails = async (req, res) => {
-//   try {
-//     const { email, name } = req.body;
-//     let user = await User.findOne({ email })
-//       .populate({
-//         path: "todos",
-//         options: { sort: { order: 1 } },
-//       })
-//       .populate({
-//         path: "completedTimers",
-//         options: { sort: { _id: -1 } },
-//       })
-//       .populate({
-//         path: "folders",
-//         populate: { path: "notes" },
-//       });
-
-//     if (!user) {
-//       user = new User({
-//         email,
-//         name,
-//       });
-//       await user.save();
-
-//       user = await User.findOne({ email }).populate({
-//         path: "folders",
-//         populate: { path: "notes" },
-//       });
-//     }
-//     const productivityData = await ProductivityData.find({ email });
-
-//     const currentDate = new Date();
-//     const todayDateString = `${("0" + currentDate.getDate()).slice(-2)}/${(
-//       "0" +
-//       (currentDate.getMonth() + 1)
-//     ).slice(-2)}/${currentDate.getFullYear()}`;
-//     const previousDate = new Date();
-//     previousDate.setDate(previousDate.getDate() - 1); // Get the previous date
-
-//     const previousDay = ("0" + previousDate.getDate()).slice(-2); // Add leading zero if needed
-//     const previousMonth = ("0" + (previousDate.getMonth() + 1)).slice(-2); // Add leading zero if needed
-//     const previousYear = previousDate.getFullYear();
-
-//     const previousDateString = `${previousDay}/${previousMonth}/${previousYear}`;
-//     const previousProductivityData = productivityData.find(
-//       (entry) => entry.date === previousDateString
-//     );
-//     const todaysProductivityData = productivityData.find(
-//       (entry) => entry.date === todayDateString
-//     );
-
-//     if (!previousProductivityData) {
-//       if (!todaysProductivityData) {
-//         user.streak = 0;
-//         await user.save();
-//       }
-//     }
-
-//     const startOfWeek = new Date();
-//     startOfWeek.setHours(0, 0, 0, 0);
-//     startOfWeek.setDate(
-//       startOfWeek.getDate() -
-//         (startOfWeek.getDay() === 0 ? 6 : startOfWeek.getDay() - 1)
-//     );
-
-//     const endOfWeek = new Date(startOfWeek);
-//     endOfWeek.setDate(endOfWeek.getDate() + 6);
-
-//     // Adjust end of week to include Sunday of next week
-//     const endOfWeekNextSunday = new Date(endOfWeek);
-//     endOfWeekNextSunday.setDate(endOfWeekNextSunday.getDate() + 1);
-
-//     const weeklySummary = [
-//       { day: "Monday", hours: 0 },
-//       { day: "Tuesday", hours: 0 },
-//       { day: "Wednesday", hours: 0 },
-//       { day: "Thursday", hours: 0 },
-//       { day: "Friday", hours: 0 },
-//       { day: "Saturday", hours: 0 },
-//       { day: "Sunday", hours: 0 },
-//     ];
-
-//     productivityData.forEach((entry) => {
-//       const { date, hoursStudied } = entry;
-//       const [day, month, year] = date.split("/");
-//       const entryDate = new Date(`${year}-${month}-${day}`);
-
-//       if (entryDate >= startOfWeek && entryDate <= endOfWeekNextSunday) {
-//         let dayOfWeek = entryDate.getDay() === 0 ? 6 : entryDate.getDay() - 1; // Adjust for Sunday as last day
-//         weeklySummary[dayOfWeek].hours += hoursStudied;
-//       }
-//     });
-
-//     const today = new Date();
-//     const currentYear = today.getFullYear();
-//     const currentMonth = today.getMonth() + 1;
-//     const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
-//     const monthlySummary = new Array(daysInMonth).fill(0).map((_, index) => ({
-//       day: index + 1,
-//       hours: 0,
-//     }));
-
-//     const yearlySummary = Array.from({ length: 12 }, (_, i) => ({
-//       month: new Date(0, i).toLocaleString("default", { month: "long" }),
-//       hours: 0,
-//     }));
-
-//     productivityData.forEach((entry) => {
-//       const { date, hoursStudied } = entry;
-//       const [day, month, year] = date.split("/");
-//       const convertedDate = new Date(`${year}-${month}-${day}`);
-
-//       if (
-//         convertedDate.getMonth() + 1 === currentMonth &&
-//         convertedDate.getFullYear() === currentYear
-//       ) {
-//         monthlySummary[convertedDate.getDate() - 1].hours += hoursStudied;
-//       }
-
-//       const monthName = new Date(convertedDate).toLocaleString("default", {
-//         month: "long",
-//       });
-//       const monthIndex = yearlySummary.findIndex(
-//         (month) => month.month === monthName
-//       );
-//       yearlySummary[monthIndex].hours += hoursStudied;
-//     });
-
-//     res.status(200).json({
-//       user: {
-//         ...user.toObject(),
-//         productivityData: {
-//           Weekly: weeklySummary,
-//           Monthly: monthlySummary,
-//           Yearly: yearlySummary,
-//         },
-//       },
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-//new getDetails method
 const getDetails = async (req, res) => {
   try {
     const { email, name, profilePicture } = req.body;
@@ -337,11 +191,10 @@ const calculateProductivityData = async (user, productivityData) => {
   const previousProductivityData = productivityData.find(
     (entry) => entry.date === previousDateString
   );
-  console.log(previousProductivityData);
+
   const todaysProductivityData = productivityData.find(
     (entry) => entry.date === todayDateString
   );
-  console.log(todaysProductivityData);
 
   if (!previousProductivityData) {
     user.streak = 0;
