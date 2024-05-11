dffdf8d979dc7e56182fffaa133ca7f890f317e5
Akshat Dubey <akshatdubey0808@gmail.com>
2024-05-11 11:52:12 +0530

    Added: A new tools navigation menu

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Navbar } from "./Components/Navigation/Navbar";
 import Loading from "./Components/Loading/Loading";
 import { loadingAtom, clientUserAtom } from "./Utils/Store";
 import { useAtom, useAtomValue } from "jotai";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   ClerkLoaded,
@@ -13,6 +13,7 @@ import {
   useUser,
 } from "@clerk/clerk-react";
 import { getUserDetails } from "./HandleApi/AuthApiHandler";
+import ToolsMenu from "./Components/Navigation/ToolsMenu";
 
 function App() {
   const navigate = useNavigate();
@@ -42,9 +43,35 @@ function App() {
     }
   }, [user, loaded, setClientUser, setLoading]);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.ctrlKey && event.key === "p") {
+        event.preventDefault();
+        setIsModalOpen((prevState) => !prevState);
+      }
+      if (event.key === "Escape") {
+        event.preventDefault();
+        setIsModalOpen(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       <div className="flex h-full min-h-screen w-full flex-col items-center bg-spaceBlack font-Poppins text-white">
+        <ToolsMenu isOpen={isModalOpen} onClose={closeModal} />
         <Toaster />
         <ClerkLoading>
           <Loading />
---
+++ Frontend/src/Components/Navigation/ToolsMenu.jsx
@@ -0,0 +1,93 @@
+import { useAtomValue } from "jotai";
+import { clientUserAtom } from "../../Utils/Store";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import toast from "react-hot-toast";
+
+export default function ToolsMenu({ isOpen, onClose }) {
+  const user = useAtomValue(clientUserAtom);
+  const inputRef = useRef(null);
+  const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredTools, setFilteredTools] = useState([]);
+
+  const toolsList = [
+    { id: 1, title: "Productivity Tracker", href: "/dashboard" },
+    { id: 2, title: "To-Do List", href: "/dashboard/todos" },
+    { id: 3, title: "Notes", href: "/dashboard/notes" },
+    { id: 4, title: "Whiteboard", href: "/dashboard/whiteboard" },
+    { id: 5, title: "Pomodoro Timer", href: "/dashboard/timer" },
+    { id: 6, title: "Friends", href: "/dashboard/friends" },
+    { id: 7, title: "Profile", href: "/dashboard/profile/" + user?.username },
+    { id: 8, title: "Settings", href: "/dashboard/edit-profile" },
+  ];
+
+  useEffect(() => {
+    if (isOpen && inputRef.current) {
+      setSearchQuery("");
+      inputRef.current.value = "";
+      inputRef.current.focus();
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    const filtered = searchQuery
+      ? toolsList.filter((tool) =>
+          tool.title.toLowerCase().includes(searchQuery.toLowerCase()),
+        )
+      : toolsList;
+    setFilteredTools(filtered);
+  }, [searchQuery]);
+
+  const handleKeyPress = (e) => {
+    if (e.key == "Enter" && filteredTools.length > 0) {
+      console.log("pressed");
+      navigate(filteredTools[0].href);
+      //   window.location.href = filteredTools[0].href;
+      onClose();
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="popup-overlay fixed inset-0 z-[51] flex justify-center"
+      onClick={onClose}
+    >
+      <div className="mt-48 h-fit w-full max-w-lg">
+        <h1 className="text-center text-3xl font-extrabold text-gray-300">
+          Tools Menu
+        </h1>
+        <input
+          ref={inputRef}
+          placeholder="Search"
+          className="bg-slate-800/ mt-4 h-10 w-full max-w-xl flex-1 rounded-lg bg-gray-100 px-3 font-bold text-zinc-700 placeholder:py-2 placeholder:font-medium focus:outline-dashed focus:outline-slate-600"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={(e) => handleKeyPress(e)}
+        />
+        <div className="mt-4 space-y-2">
+          {filteredTools.map((tool) => (
+            <Link
+              onClick={onClose}
+              to={tool.href}
+              key={tool.id}
+              className="flex cursor-pointer items-center justify-between rounded-lg bg-gray-100 px-4 py-2 font-bold text-zinc-700 hover:bg-white/90"
+            >
+              {tool.title}
+            </Link>
+          ))}
+        </div>
+        <div className="mt-4 flex items-center justify-center">
+          <button
+            className="rounded-lg bg-white px-4 py-1.5 font-medium text-black"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
